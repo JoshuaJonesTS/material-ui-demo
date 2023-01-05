db466df2dd17df5f7bde776e7b7c62699cb2047c
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-05 15:45:29 -0800

    Use import to change font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Typography } from '@material-ui/core'; // Typography is any text element
+import { Typography } from '@mui/material'; // Typography is any text element
 
 const App = () => {
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <Typography variant="h1">Hello, World!</Typography>
         </div>
     );
 }
