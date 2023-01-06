745bfbb6496141d44f071d1c574f95e1b69f689b
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-05 20:21:13 -0800

    Set up code for CSS (MUI)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material'; // Typography is any text element
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material'; // Typography is any text element
 // import {PhotoCameraIcon} from '@mui/icons-material/PhotoCamera';
+import { makeStyles } from '@mui/styles';
 
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+const useStyles = makeStyles((theme) => ({
+    container: {
+        // backgroundColor: theme.palette.background.paper
+        // padding: theme.spacing(8, 0, 6)
+    }
+}));
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <>
             <CssBaseline/>
             <AppBar position="relative">
                 <Toolbar>
-                    {/* <PhotoCameraIcon/> */}
                     <Typography variant="h6">
                         Photo Album
                     </Typography>
                 </Toolbar>
             </AppBar>
             <main>
-                <div>
+                <div className={classes.container}>
                     <Container maxWidth="sm">
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Photo Album
@@ -26,13 +32,22 @@ const App = () => {
                         <Typography variant="h5" align="center" color="textPrimary" paragraph>
                             Hello everyone! This is a photo album.
                         </Typography>
+                        <div>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        See my photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
-                <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                    <Button>One</Button>
-                    <Button>Two</Button>
-                    <Button>Three</Button>
-                </ButtonGroup>
             </main>
         </>
     );
