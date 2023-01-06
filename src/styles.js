9cb6dc1fadd8ca124842832338690a47730ec5c4
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-06 15:52:59 -0800

    Style grid image

--- src/styles.js
+++ src/styles.js
@@ -7,6 +7,20 @@ const useStyles = makeStyles(({
     },
     button: {
         marginTop: '40px',
+    },
+    cardGrid: {
+        padding: '20px 0'
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    cardMedia: {
+        paddingTop: '56.25%'
+    },
+    cardContent: {
+        flexGrow: 1
     }
 }));
 
