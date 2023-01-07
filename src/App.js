d9a6a003a889d8ba33fd22e968fd8a75d1188d81
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-06 16:42:14 -0800

    Add responsiveness to larger screens

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4}>
                         {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6}>
+                            <Grid item key={card} xs={12} sm={6} md={4}>
                             <Card className={classes.card}>
                                 <CardMedia
                                     className={classes.cardMedia}
