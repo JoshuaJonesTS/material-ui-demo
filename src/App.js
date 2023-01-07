76c546be051daa57fcca72326a0140708775d386
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-06 16:40:01 -0800

    Mobile responsive image size

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4}>
                         {cards.map((card) => (
-                            <Grid item key={card}>
+                            <Grid item key={card} xs={12} sm={6}>
                             <Card className={classes.card}>
                                 <CardMedia
                                     className={classes.cardMedia}
